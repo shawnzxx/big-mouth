4d451c1d5656aaffc8c9410f8070655d074d08de
Shawn Zhang <shawn.zhang@razerzone.com>
2018-06-27 00:13:43 +0800

    Can pass all acceptance test

--- big-mouth/tests/steps/init.js
+++ big-mouth/tests/steps/init.js
@@ -18,8 +18,9 @@ let init = co.wrap(function* () {
     process.env.restaurants_api = "https://d8rqdddzxj.execute-api.us-east-1.amazonaws.com/dev/restaurants";
     process.env.restaurants_table = "restaurants";
     process.env.AWS_REGION = "us-east-1";
-    process.env.cognito_user_pool_id = "dummy_cognito_user_pool_id";
-    process.env.cognito_client_id = "dummy_cognito_client_id";
+    process.env.cognito_client_id = "4bhgqbem4f6p8leiq4fmnals1m";
+    process.env.cognito_user_pool_id = "us-east-1_BXJNx0zbs";
+    process.env.cognito_server_client_id = '6l978fn8f91ajhht4of78mfjl4';
 
     let cred = (yield awscred.loadAsync()).credentials;
 
--- big-mouth/tests/test-cases/search-restaurants.js
+++ big-mouth/tests/test-cases/search-restaurants.js
@@ -7,21 +7,27 @@
  const expect = require('chai').expect;
  const init = require('../steps/init');
  const when = require('../steps/when');
+ const given = require('../steps/given');
 
-describe("When we invoke the POST /restaurants/search endpoint with theme 'cartoon'", co.wrap(function* () {
+describe('Given an authentication user', co.wrap(function* () {
+    let user;
     before(co.wrap(function* () {
         yield init.init();
+        user = yield given.an_authenticated_user();
     }));
 
-    it('should return an array of 4 restaurants', co.wrap(function* () {
-        let res = yield when.we_invoke_search_restaurants('cartoon');
+    describe("When we invoke the POST /restaurants/search endpoint with theme 'cartoon'", co.wrap(function* () {
+        it('should return an array of 4 restaurants', co.wrap(function* () {
+            let res = yield when.we_invoke_search_restaurants(user, 'cartoon');
 
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.have.lengthOf(4);
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.have.lengthOf(4);
 
-        for (let restaurant of res.body){
-            expect(restaurant).to.have.property('name');
-            expect(restaurant).to.have.property('image');
-        }
+            for (let restaurant of res.body){
+                expect(restaurant).to.have.property('name');
+                expect(restaurant).to.have.property('image');
+            }
+        }));
     }));
 }));
+
