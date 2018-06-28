68cf074b79989f7fd52040b88d508d8a736fc411
shawnzxx <shawnzhang.dev@gmail.com>
2018-06-28 11:11:46 +0800

    Change to use Visual Studio Code debugging our API

--- big-mouth/tests/steps/init.js
+++ big-mouth/tests/steps/init.js
@@ -6,7 +6,6 @@
 
 const co = require('co');
 const Promise = require('bluebird');
-const awscred = Promise.promisifyAll(require('awscred'));
 
 let initialized = false;
 
@@ -21,11 +20,6 @@ let init = co.wrap(function* () {
     process.env.cognito_client_id = "4bhgqbem4f6p8leiq4fmnals1m";
     process.env.cognito_user_pool_id = "us-east-1_BXJNx0zbs";
     process.env.cognito_server_client_id = '6l978fn8f91ajhht4of78mfjl4';
-
-    let cred = (yield awscred.loadAsync()).credentials;
-
-    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
-    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
 
     initialized = true;
 });
