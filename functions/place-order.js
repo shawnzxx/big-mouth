a2998da72f1181120d7d637e5d0d84db30d9fee5
Shawn Zhang <shawn.zhang@razerzone.com>
2018-07-06 22:56:38 +0800

    Add kinesis place_order

---
+++ functions/place-order.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const co = require('co');
+const AWS = require('aws-sdk');
+const Kinesis = new AWS.Kinesis();
+const chance = require('chance').Chance();
+const streamName = process.env.order_events_stream;
+
+module.exports.handler = co.wrap(function* (event, context, cb){
+    let restaurantName = JSON.parse(event.body).restaurantName;
+
+    let userEmail = event.requestContext.authorizer.claims.email;
+
+    let orderId = chance.guid();
+    console.log(`placing order ID [${orderId}] to [${restaurantName}] from user [${userEmail}]`);
+
+    let data = {
+        orderId,
+        userEmail,
+        restaurantName,
+        eventType: 'order_placed'
+    };
+
+    let putReq = {
+        Data: JSON.stringify(data),
+        PartitionKey: orderId,
+        StreamName: streamName
+    };
+    yield Kinesis.putRecord(putReq).promise();
+
+    console.log("published 'order_placed' event into Kinesis");
+
+    let response = {
+        statusCode: 200,
+        body: JSON.stringify( {orderId} )
+    };
+    cb(null, response);
+})
