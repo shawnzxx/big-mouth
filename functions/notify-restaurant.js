5dd1a5f1b154ae7e57768c548e5bf3aa4737ea69
Shawn Zhang <shawn.zhang@razerzone.com>
2018-07-21 01:02:17 +0800

    partially retry

--- functions/notify-restaurant.js
+++ functions/notify-restaurant.js
@@ -1,36 +1,22 @@
-'use strict'
+'use strict';
 
-const _ = require('lodash');
 const co = require('co');
-const AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-const sns = new AWS.SNS();
-const streamName = process.env.order_events_stream;
 const getRecords = require('../lib/kinesis').getRecords;
-const topicArn = process.env.restaurant_notification_topic;
+const notify = require('../lib/notify');
+const retry = require('../lib/retry');
 
 module.exports.handler = co.wrap(function* (event, context, cb) {
     let records = getRecords(event);
     let orderPlaced = records.filter(r => r.eventType === 'order_placed');
-
-    for(let order of orderPlaced){
-        let pubReq = {
-            Message: JSON.stringify(order),
-            TopicArn: topicArn
-        };
-        yield sns.publish(pubReq).promise();
-        console.log(`notified restaurant [${order.restaurantName}] of order [${order.orderId}]`);
-
-        let data = _.clone(order);
-        data.eventType = 'restaurant_notified';
-
-        let kinesisReq = {
-            Data: JSON.stringify(data), // the SDK would base64 encode this for us
-            PartitionKey: order.orderId,
-            StreamName: streamName
-        };
-        yield Kinesis.putRecord(kinesisReq).promise();
-        console.log(`published 'restaurant_notified' event to Kinesis`);
+  
+    for (let order of orderPlaced) {
+      try {
+        yield notify.restaurantOfOrder(order);
+        cb(null, "all done");
+      } catch (err) {
+          console.log(`notify-restaurant error log: ${err}`);
+          yield retry.restaurantNotification(order);
+          cb(err);
+      }
     }
-    cb(null, "all done");
-})
+  });
