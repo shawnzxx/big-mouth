17eb3c59523972d6352a5448783cc4585107aa92
Shawn Zhang <shawn.zhang@razerzone.com>
2018-07-22 23:36:41 +0800

    update

--- functions/notify-restaurant.js
+++ functions/notify-restaurant.js
@@ -12,11 +12,11 @@ module.exports.handler = co.wrap(function* (event, context, cb) {
     for (let order of orderPlaced) {
       try {
         yield notify.restaurantOfOrder(order);
-        cb(null, "all done");
       } catch (err) {
           console.log(`notify-restaurant error log: ${err}`);
-          yield retry.restaurantNotification(order);
-          cb(err);
+          yield retry.restaurantNotification(order);  //failed case after insert retry message to the queue go outside call cb success
+          // cb(err); // here can not throw error, otherwise lambda will loop invoke notify-restaurant again and agian, will send multiple DLQ email
       }
     }
+    cb(null, "all done");
   });
